00a541c7ddb920dc7562dd504ce518c6c4d71967
Doukkali El Uahabi Rihab <doukkali-el-u1@mmi-dev.unilim.fr>
2025-04-03 10:08:26 +0200

    update credentials

--- server/model.php
+++ server/model.php
@@ -14,6 +14,6 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "mora");
+define("DBNAME", "doukkali-el-u1");
 define("DBLOGIN", "root");
 define("DBPWD", "root");
